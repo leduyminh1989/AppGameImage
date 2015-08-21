ada079b805c51a0287c9ed1d2f80424f651ad018
Le Duy Minh <leduyminh2521989@gmail.com>
2015-08-21 22:22:35 +0700

    Setup Project

---
+++ TestImageSoln/FindDiffPoints/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace ImageTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClick = new System.Windows.Forms.Button();
+            this.linkImage1 = new System.Windows.Forms.LinkLabel();
+            this.linkImage2 = new System.Windows.Forms.LinkLabel();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.lblIma1 = new System.Windows.Forms.Label();
+            this.lblIma2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnClick
+            // 
+            this.btnClick.Location = new System.Drawing.Point(16, 59);
+            this.btnClick.Name = "btnClick";
+            this.btnClick.Size = new System.Drawing.Size(75, 23);
+            this.btnClick.TabIndex = 0;
+            this.btnClick.Text = "Click";
+            this.btnClick.UseVisualStyleBackColor = true;
+            this.btnClick.Click += new System.EventHandler(this.btnClick_Click);
+            // 
+            // linkImage1
+            // 
+            this.linkImage1.AutoSize = true;
+            this.linkImage1.Location = new System.Drawing.Point(13, 13);
+            this.linkImage1.Name = "linkImage1";
+            this.linkImage1.Size = new System.Drawing.Size(78, 13);
+            this.linkImage1.TabIndex = 1;
+            this.linkImage1.TabStop = true;
+            this.linkImage1.Text = "Select Image 1";
+            this.linkImage1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkImage1_LinkClicked);
+            // 
+            // linkImage2
+            // 
+            this.linkImage2.AutoSize = true;
+            this.linkImage2.Location = new System.Drawing.Point(13, 33);
+            this.linkImage2.Name = "linkImage2";
+            this.linkImage2.Size = new System.Drawing.Size(78, 13);
+            this.linkImage2.TabIndex = 2;
+            this.linkImage2.TabStop = true;
+            this.linkImage2.Text = "Select Image 2";
+            this.linkImage2.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkImage2_LinkClicked);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar1.Location = new System.Drawing.Point(16, 95);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(512, 23);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(16, 124);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(512, 350);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            // 
+            // lblIma1
+            // 
+            this.lblIma1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblIma1.AutoSize = true;
+            this.lblIma1.Location = new System.Drawing.Point(97, 13);
+            this.lblIma1.Name = "lblIma1";
+            this.lblIma1.Size = new System.Drawing.Size(23, 13);
+            this.lblIma1.TabIndex = 5;
+            this.lblIma1.Text = "null";
+            // 
+            // lblIma2
+            // 
+            this.lblIma2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblIma2.AutoSize = true;
+            this.lblIma2.Location = new System.Drawing.Point(97, 33);
+            this.lblIma2.Name = "lblIma2";
+            this.lblIma2.Size = new System.Drawing.Size(23, 13);
+            this.lblIma2.TabIndex = 6;
+            this.lblIma2.Text = "null";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(540, 486);
+            this.Controls.Add(this.lblIma2);
+            this.Controls.Add(this.lblIma1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.linkImage2);
+            this.Controls.Add(this.linkImage1);
+            this.Controls.Add(this.btnClick);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClick;
+        private System.Windows.Forms.LinkLabel linkImage1;
+        private System.Windows.Forms.LinkLabel linkImage2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.Label lblIma1;
+        private System.Windows.Forms.Label lblIma2;
+    }
+}
+
---
+++ TestImageSoln/FindDiffPoints/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblIma1.Text = Path.Combine(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location), @"Images\Ima1.png");
+            lblIma2.Text = Path.Combine(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location), @"Images\Ima2.png");
+            progressBar1.Visible = false;
+            pictureBox1.Visible = false;
+        }
+
+        private void btnClick_Click(object sender, EventArgs e)
+        {
+            progressBar1.Visible = true;
+            string img1_ref, img2_ref;
+            var img1 = new Bitmap(lblIma1.Text);
+            var img2 = new Bitmap(lblIma2.Text);
+            var img3 = GrayScaleFilter(img1);
+
+            progressBar1.Maximum = img1.Width;
+            bool flag = true;
+            if (img1.Width == img2.Width && img1.Height == img2.Height)
+            {
+                for (int i = 0; i < img1.Width; i++)
+                {
+                    for (int j = 0; j < img1.Height; j++)
+                    {
+                        img1_ref = img1.GetPixel(i, j).ToString();
+                        img2_ref = img2.GetPixel(i, j).ToString();
+
+                        if (img1_ref != img2_ref)
+                        {
+                            flag = false;
+                            img3.SetPixel(i, j, Color.Red);
+                        }
+                    }
+                    progressBar1.Value++;
+                }
+                if (flag == false)
+                {
+                    pictureBox1.Visible = true;
+                    pictureBox1.Width = img3.Width;
+                    pictureBox1.Height = img3.Height;
+                    pictureBox1.Image = img3;
+                }
+                else
+                    MessageBox.Show("Images are same !");
+            }
+            else
+                MessageBox.Show("Can not compare this images");
+        }
+
+        public Bitmap GrayScaleFilter(Bitmap image)
+        {
+            Bitmap grayScale = new Bitmap(image.Width, image.Height);
+
+            for (Int32 y = 0; y < grayScale.Height; y++)
+                for (Int32 x = 0; x < grayScale.Width; x++)
+                {
+                    Color c = image.GetPixel(x, y);
+
+                    Int32 gs = (Int32)(c.R * 0.3 + c.G * 0.59 + c.B * 0.11);
+
+                    grayScale.SetPixel(x, y, Color.FromArgb(gs, gs, gs));
+                }
+            return grayScale;
+        }
+
+        private void linkImage1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            openFileDialog1.FileName = "";
+            openFileDialog1.Title = "Images";
+            openFileDialog1.Filter = "All Images|*.jpg; *.bmp; *.png";
+            openFileDialog1.ShowDialog();
+            if (openFileDialog1.FileName.ToString() != "")
+            {
+                lblIma1.Text = openFileDialog1.FileName.ToString();
+            }
+        }
+
+        private void linkImage2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            openFileDialog2.FileName = "";
+            openFileDialog2.Title = "Images";
+            openFileDialog2.Filter = "All Images|*.jpg; *.bmp; *.png";
+            openFileDialog2.ShowDialog();
+            if (openFileDialog2.FileName.ToString() != "")
+            {
+                lblIma2.Text = openFileDialog2.FileName.ToString();
+            }
+        }
+    }
+}
